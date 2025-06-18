633e7b41b74ba2d3315b33763ffaee6a2966d6c4
phanunsuon <phanunsuon@gmail.com>
2025-06-18 08:30:59 +0000

    modified:   app/dashboard/products/page.jsx 	modified:   app/i18n/index.js 	modified:   app/i18n/settings.js
    Changes not staged for commit:
    	modified:   app/dashboard/addresses/page.jsx
    	modified:   app/dashboard/branches/page.jsx
    	modified:   app/dashboard/brands/page.jsx
    	modified:   app/dashboard/categories/page.jsx
    	modified:   app/dashboard/payments/page.jsx
    	modified:   app/dashboard/reports/page.jsx
    	modified:   app/dashboard/settings/page.jsx
    	modified:   components/header.jsx
    	modified:   components/sidebar.jsx
    	modified:   public/locales/cn/common.json
    	modified:   public/locales/en/common.json
    	deleted:    public/locales/en/translation.json
    	modified:   public/locales/kh/common.json

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -19,22 +19,3 @@ i18n
   });
 
 export default i18n;
-
-
-
-// import i18n from 'i18next';
-// import { initReactI18next } from 'react-i18next';
-
-// i18n
-//   .use(initReactI18next)
-//   .init({
-//     fallbackLng: 'en',
-//     interpolation: {
-//       escapeValue: false,
-//     },
-//     backend: {
-//       loadPath: '/locales/{{lng}}/{{ns}}.json',
-//     },
-//   });
-
-// export default i18n;
--- app/i18n/settings.js
+++ app/i18n/settings.js
@@ -26,32 +26,3 @@ export const i18n = {
 };
 
 
-
-
-
-// export const languages = ['en', 'kh', 'cn']; 
-// export const defaultLanguage = 'en';
-// export const cookieName = 'i18next';
-
-// export function getOptions(lng = defaultLanguage, ns = 'common') {
-//     return {
-//         supportedLngs: languages,
-//         fallbackLng: defaultLanguage,
-//         lng,
-//         ns,
-//         defaultNS: 'common',
-//         fallbackNS: 'common',
-//         backend: {
-//             loadPath: '/locales/{{lng}}/{{ns}}.json',
-//         },
-//         interpolation: {
-//             escapeValue: false,
-//         },
-//     };
-// }
-
-// export const i18n = {
-//     defaultLocale: defaultLanguage,
-//     locales: languages, 
-//     localeDetection: true,
-// };
